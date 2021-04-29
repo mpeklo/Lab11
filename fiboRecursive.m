39f014b1d9a7519a3d50b3be968f344fc2d3facd
Maggie Peklo <maggiepeklo@maggies-mbp-4.unl.edu>
2021-04-28 23:16:19 -0500

    we're adding the fibonacchi here because for some reason it won't let me leave

---
+++ fiboRecursive.m
@@ -0,0 +1,15 @@
+function fibonacciNumber = fiboRecursive(n)
+
+global count;
+if n == 1
+    fibonacciNumber = 1;
+elseif n == 2
+    fibonacciNumber = 1;
+else
+    count = count + 1;
+   fibonnacciNumber = fiboRecursive(n-2) + fibRecursive(n-1);
+end
+
+
+
+end
